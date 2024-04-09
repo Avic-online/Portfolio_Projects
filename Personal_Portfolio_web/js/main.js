da665323309a61fad72f8c91e7992652ccb213af
Avic-online <avicdon1@gmail.com>
2024-04-09 14:39:41 +0100

    nav and header coding
    Signed-off-by: Avic-online <avicdon1@gmail.com>

---
+++ Personal_Portfolio_web/js/main.js
@@ -0,0 +1,14 @@
+// javascript code for our portfolio site
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sideBar');
+    sidebar.style.display = 'flex'
+}
+
+
+// function to hide side bar when close icon is clicked
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sideBar');
+    sidebar.style.display = 'none'
+}
