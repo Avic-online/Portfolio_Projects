cf40a5c1f58e1d6a69ab60ccc258b4998886145b
Avic-online <avicdon1@gmail.com>
2024-04-13 20:02:21 +0100

    embedding of google map to contact page
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- Personal_Portfolio_web/js/main.js
+++ Personal_Portfolio_web/js/main.js
@@ -12,3 +12,13 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sideBar');
     sidebar.style.display = 'none'
 }
+
+
+// function to embed map on the contact me page
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map-container'), {
+      center: {lat: -34.397, lng: 150.644}, 
+      zoom: 8 
+    });
+  }
